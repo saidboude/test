1d7489e8eadd5fdbefeda079c9097d27c1a98f50
saidboude <said_boudhane@hotmail.com>
2022-01-19 14:42:36 -0500

    Nettoyer le code

--- script3.sh
+++ script3.sh
@@ -1,97 +1,64 @@
 #!/bin/bash
 
-set -x
-
-# Token :
-# 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
-#
-
 #
 # Verifier nombre de parametres passer au script
 #
 function verifierParameteres(){
   local nombreParams=$1
-  
-  echo "Nombre des Parametres = ${nombreParams}"
-
+  #echo "Nombre des Parametres = ${nombreParams}"
   if [[ ${nombreParams} == 7 ]]; then
-    echo "Debut de traitement ..."
     return 0;
   else
-    echo "ERREUR : Il faut passer 7 nombre en paramwetres."
+    echo "ERREUR : Il faut passer 7 nombres en parametres."
     return 1;
   fi
 }
 
-
 #
 # Fonction plusgrand() : 
-#     Recoit une liste de 7 nombres et elle affiche tous
+#     Recoit une liste de 7 nombres en parameteres et elle affiche tous
 #     les nombres qui  sont plus grandes que le premier nombre.  
 #
 function plusgrands(){
-  local reference=$1
-  local nombre_2=$2
-  local nombre_3=$3
-  local nombre_4=$4
-  local nombre_5=$5
-  local nombre_6=$6
-  local nombre_7=$7
-
-  liste=${@}
-  echo "Liste = ${liste}"
-
-  for in var in "${liste}"; do
-     echo "$var";
-  done
   
-#  if [[ ${#} == 7 ]]; then
-#    echo "Debut de traitement ..."
-#  else
-#    echo "ERREUR : Il faut passer 7 nombre en paramwetres."
-#  fi
-
+  local liste=("${@}")
+  local reference=0
+  for index in ${!liste[@]}; do
+    if [[ $index == 0 ]]; then
+      reference=${liste[0]}
+    else
+      local valeur="${liste[$index]}"
+      if [[ ${valeur} -gt "${reference}" ]]; then
+        echo "${valeur}"  
+      fi  
+    fi
+  done
 }
 
-##############################################
+#############################################################
 #
-## Main
+## Main :
+#    Le script recoit une liste de 7 nombres. Il affiche
+#    tous les nombres qui  sont plus grandes que le premier
+#    nombre.
 #
-##############################################
-echo Hello World !!
+#############################################################
 
-# Verifier Parameteres
+# Verifier les parameteres
 verifierParameteres $#
-
-liste="$@"
-for var in "$liste"
-do
-    echo "$var"
-done
-
-plusgrands $@
 
 # Parametres valides
 if [[ ${?} == 0 ]]; then
 
- # Reccuperer les deux nombres
- nombre_1=$1
- nombre_2=$2
+  # Traitement
+  plusgrands $@
 
- # Il faut verifier qu ils sont numeriques !!
- # TO DO .....
+  # Fin avec success
+  exit 0;
 
- echo "nombre_1= = ${nombre_1}"
- echo "nombre_2= = ${nombre_2}"
- 
- # Retourner le Max
- if [[ ${nombre_1} < ${nombre_1} ]]; then
-   max=${nombre_1}
- else
-   max=${nombre_2}
- fi
+else
 
- # Afficher le max
- echo "La plus grande valeur entre ${nombre_1} et ${nombre_2} c'est ${max}."
+  # Fin avec erreur
+  exit 1;
 
 fi
